b0a55ab52bbfdb91f4d0458fef1c09beccaea954
Sebastian Ramirez <115853734+sbstnrmrz@users.noreply.github.com>
2023-12-29 12:57:15 -0400

    Delete nvim directory

--- nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require("sebas.settings")
-require("sebas.remaps")
-require("sebas.lazy")
-
-vim.cmd("colorscheme gruvbox")
---vim.cmd.colorscheme "catppuccin"
---vim.cmd[[colorscheme gruvbox-material]]
